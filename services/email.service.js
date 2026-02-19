44606387e2937cf88a9362a9064c39c296853b4d
Omkar Kamat <mail4omkarkamat@gmail.com>
2026-02-19 12:07:02 +0530

    email debug

--- services/email.service.js
+++ services/email.service.js
@@ -5,16 +5,15 @@ import getTransporter from "../config/mailer.js";
 class EmailService {
     static async sendOtpEmail(to, otp) {
         const mailOptions = {
-            from: process.env.EMAIL_ID,
+            from: `"creatorConnect" ${process.env.EMAIL_ID}`,
             to,
             subject: "Your CreatorConnect Verification Code",
-            text: `Your OTP is ${otp}`
-    //         html: `
-    //     <h3>Verification Code</h3>
-    //     <p>Your OTP is:</p>
-    //     <h2>${otp}</h2>
-    //     <p>This code will expire in 10 minutes.</p>
-    //   `,
+            html: `
+        <h3>Verification Code</h3>
+        <p>Your OTP is:</p>
+        <h2>${otp}</h2>
+        <p>This code will expire in 10 minutes.</p>
+      `,
         };
 
         const transporter = getTransporter();
